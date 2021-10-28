2be574f4eb486a4354ab3faef35c18cc764a94a8
Sergey Dotsenko <s.docent@gmail.com>
2021-10-28 01:21:38 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleDocker
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduledocker/uk/

--- Messages/uk.php
+++ Messages/uk.php
@@ -1,1 +1,16 @@
 <?php
+return [
+    'mod_dkr_Disconnected' => 'Модуль Docker вимкнено',
+    'mod_dkr_Connected' => 'Модуль Docker підключено',
+    'SubHeaderModuleDocker' => 'Підтримка Docker контейнерів для сторонніх модулів',
+    'BreadcrumbModuleDocker' => 'Модуль Docker',
+    'mo_ModuleModuleDocker' => 'Модуль Docker',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleDocker' => 'Модуль Docker - %repesent%',
+];
