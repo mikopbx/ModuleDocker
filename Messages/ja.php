ab9c24909b109c0da7da41cd5c6da3ebd8932b30
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:25:08 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleDocker
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduledocker/ja/

--- Messages/ja.php
+++ Messages/ja.php
@@ -1,1 +1,16 @@
 <?php
+return [
+    'SubHeaderModuleDocker' => 'サードパーティモジュール用のDockerコンテナサポート',
+    'BreadcrumbModuleDocker' => 'Dockerモジュール',
+    'mo_ModuleModuleDocker' => 'Dockerモジュール',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleDocker' => 'Dockerモジュール-％repesent％',
+    'mod_dkr_Disconnected' => 'Dockerモジュールが無効になっています',
+    'mod_dkr_Connected' => 'Dockerモジュールが接続されました',
+];
