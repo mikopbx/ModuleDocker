196b7da6c7f11dccc71030e43ea2ad1b6cb65105
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:26:52 +0000

    Translated using Weblate (Georgian)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleDocker
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduledocker/ka/

--- Messages/ka.php
+++ Messages/ka.php
@@ -1,1 +1,16 @@
 <?php
+return [
+    'mod_dkr_Disconnected' => 'დოკერის მოდული გამორთულია',
+    'mod_dkr_Connected' => 'Docker მოდული დაკავშირებულია',
+    'SubHeaderModuleDocker' => 'Docker კონტეინერის მხარდაჭერა, მესამე მხარის მოდულებისთვის',
+    'BreadcrumbModuleDocker' => 'დოკერის მოდული',
+    'mo_ModuleModuleDocker' => 'დოკერის მოდული',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleDocker' => 'დოკერის მოდული -% რეპესენტი%',
+];
