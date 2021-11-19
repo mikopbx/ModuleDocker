b49d1ea371a023bdbf59287dcf33cca55b9fa76a
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:21:32 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleDocker
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduledocker/vi/

--- Messages/vi.php
+++ Messages/vi.php
@@ -1,1 +1,16 @@
 <?php
+return [
+    'SubHeaderModuleDocker' => 'Hỗ trợ vùng chứa Docker, dành cho các mô-đun của bên thứ ba',
+    'BreadcrumbModuleDocker' => 'Mô-đun Docker',
+    'mo_ModuleModuleDocker' => 'Mô-đun Docker',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleDocker' => 'Mô-đun Docker -% repesent%',
+    'mod_dkr_Disconnected' => 'Mô-đun Docker bị vô hiệu hóa',
+    'mod_dkr_Connected' => 'Đã kết nối mô-đun Docker',
+];
