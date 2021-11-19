d5171ec50a611dffa0e810e6ea15d67e1ac63f94
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:23:12 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleDocker
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduledocker/fr/

--- Messages/fr.php
+++ Messages/fr.php
@@ -1,1 +1,16 @@
 <?php
+return [
+    'SubHeaderModuleDocker' => 'Prise en charge des conteneurs Docker, pour les modules tiers',
+    'BreadcrumbModuleDocker' => 'Module Docker',
+    'mo_ModuleModuleDocker' => 'Module Docker',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleDocker' => 'Module Docker -% représentant%',
+    'mod_dkr_Disconnected' => 'Module Docker désactivé',
+    'mod_dkr_Connected' => 'Module Docker connecté',
+];
