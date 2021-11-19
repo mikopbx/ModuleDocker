bc5dfdef110a3acbe0366ed846ffba476990185d
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:18:25 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleDocker
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduledocker/cs/

--- Messages/cs.php
+++ Messages/cs.php
@@ -1,1 +1,16 @@
 <?php
+return [
+    'SubHeaderModuleDocker' => 'Podpora kontejnerů Docker pro moduly třetích stran',
+    'BreadcrumbModuleDocker' => 'Docker modul',
+    'mo_ModuleModuleDocker' => 'Docker modul',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleDocker' => 'Modul Docker -% reprezentativní%',
+    'mod_dkr_Disconnected' => 'Modul Docker je zakázán',
+    'mod_dkr_Connected' => 'Docker modul připojen',
+];
