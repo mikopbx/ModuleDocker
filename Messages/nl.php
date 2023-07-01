3f8c79f9bae9c49ff3ce08454da1c0c082da961c
Jochem Pluim <jochem@cafedoranjeboom.nl>
2023-07-01 23:19:00 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleDocker
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduledocker/nl/

--- Messages/nl.php
+++ Messages/nl.php
@@ -1,1 +1,16 @@
 <?php
+return [
+    'mod_dkr_Disconnected' => 'Docker niet-actief',
+    'mod_dkr_Connected' => 'Docker actief',
+    'SubHeaderModuleDocker' => 'Docker-containermanager',
+    'BreadcrumbModuleDocker' => 'Docker',
+    'mo_ModuleModuleDocker' => 'Docker',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleDocker' => 'Docker module - %repesent%',
+];
