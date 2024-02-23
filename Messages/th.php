9f5b6351440c00c278e1f823b8078e2fc0e5a4bc
Anonymous <noreply@weblate.org>
2024-02-23 02:57:42 +0000

    Translated using Weblate (Thai)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleDocker
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduledocker/th/

--- Messages/th.php
+++ Messages/th.php
@@ -1,1 +1,16 @@
 <?php
+return [
+    'mo_ModuleModuleDocker' => 'โมดูลนักเทียบท่า',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleDocker' => 'โมดูลนักเทียบท่า - % ซ้ำ%',
+    'BreadcrumbModuleDocker' => 'โมดูลนักเทียบท่า',
+    'SubHeaderModuleDocker' => 'รองรับคอนเทนเนอร์ Docker สำหรับโมดูลบุคคลที่สาม',
+    'mod_dkr_Connected' => 'เชื่อมต่อโมดูลนักเทียบท่าแล้ว',
+    'mod_dkr_Disconnected' => 'โมดูลนักเทียบท่าถูกปิดใช้งาน',
+];
