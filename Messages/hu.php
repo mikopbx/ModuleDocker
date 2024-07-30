51d9570b6495462b1a0c3b48c2f1b6fb36e00b9a
Anonymous <noreply@weblate.org>
2024-07-30 07:23:57 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleDocker
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduledocker/hu/

--- Messages/hu.php
+++ Messages/hu.php
@@ -1,1 +1,16 @@
 <?php
+return [
+    'SubHeaderModuleDocker' => 'Docker konténerek támogatása harmadik féltől származó modulokhoz',
+    'mod_dkr_Connected' => 'Docker modul csatlakoztatva',
+    'mod_dkr_Disconnected' => 'Docker modul letiltva',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleDocker' => 'Docker modul – %repesent%',
+    'mo_ModuleModuleDocker' => 'Docker modul',
+    'BreadcrumbModuleDocker' => 'Docker modul',
+];
