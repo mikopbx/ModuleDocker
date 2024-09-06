17250e0d1452b266575acdb51d8aac780ef23e00
Anonymous <noreply@weblate.org>
2024-09-06 04:00:50 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleDocker
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduledocker/fi/

--- Messages/fi.php
+++ Messages/fi.php
@@ -1,1 +1,16 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleDocker' => 'Docker-moduuli - %repesent%',
+    'mo_ModuleModuleDocker' => 'Docker-moduuli',
+    'BreadcrumbModuleDocker' => 'Docker-moduuli',
+    'SubHeaderModuleDocker' => 'Tuki Docker-säiliöille, kolmannen osapuolen moduuleille',
+    'mod_dkr_Connected' => 'Docker-moduuli kytketty',
+    'mod_dkr_Disconnected' => 'Docker-moduuli poistettu käytöstä',
+];
