bf252414afe30d6e1e3313ca732959b053ef8c12
Anonymous <noreply@weblate.org>
2024-11-18 23:04:35 +0000

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleDocker
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduledocker/hr/

--- Messages/hr.php
+++ Messages/hr.php
@@ -1,1 +1,16 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 11 2019
+ *
+ */
+    'repModuleDocker' => 'Docker modul - %repesent%',
+    'mo_ModuleModuleDocker' => 'Docker modul',
+    'BreadcrumbModuleDocker' => 'Docker modul',
+    'SubHeaderModuleDocker' => 'Podrška za Docker spremnike, za module trećih strana',
+    'mod_dkr_Connected' => 'Docker modul povezan',
+    'mod_dkr_Disconnected' => 'Docker modul onemogućen',
+];
